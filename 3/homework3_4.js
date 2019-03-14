ee4fc679a66eb2c0b89ad4a96d2d002361e30169
Pun <pjingjai@gmail.com>
2019-03-14 23:05:14 +0700

    change 'newObj'

--- 3/homework3_4.js
+++ 3/homework3_4.js
@@ -2,18 +2,17 @@ function clone(obj) {
     if (typeof obj !== 'object') {
         return obj;
     }
+
+    let newObj = {};
     if (obj instanceof Array) {
-        let newArr = [];
-        for (i in obj) {
-            newArr[i] = clone(obj[i]);
-        }
-        return newArr;
-    }
-     else {
-        let newObj = {};
         for (i in obj) {
             newObj[i] = clone(obj[i]);
         }
-        return newObj;
     }
+     else {
+        for (i in obj) {
+            newObj[i] = clone(obj[i]);
+        }
+    }
+    return newObj;
 }
