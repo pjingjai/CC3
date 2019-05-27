d2c2fe508432761a21642b66b1701aebc6f1040a
Pun <pjingjai@gmail.com>
2019-05-27 20:50:15 +0700

    hw20_1

--- 20/homework20_1.js
+++ 20/homework20_1.js
@@ -1,18 +1,21 @@
 const Koa = require('koa')
 const session = require('koa-session')
+const Router = require('koa-router')
 const mysql = require('mysql2/promise')
 const render = require('koa-ejs')
+const path = require('path')
 const koaBody = require('koa-body')
 
 const app = new Koa()
+const router = new Router()
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'codecamp'
+    database: 'pikkanode'
 })
 render(app, {
-    root: __dirname,
+    root: path.join(__dirname, '/views'),
     layout: false,
     viewExt: 'ejs',
     cache: false
@@ -21,13 +24,36 @@ render(app, {
 app.keys = ['supersecret'];
 const sessionConfig = {
     key: 'sess',
-    maxAge: 2 * 1000 * 60,
+    maxAge: 3 * 1000 * 60,
     httpOnly: true
 };
 app.use(session(sessionConfig, app));
 
-async function fb(ctx) {
-    // ctx.request.session = facebook_userId
-}
+router.get('/', async ctx => {
+    await ctx.render('fb')
+});
+router.post('/', koaBody(), async ctx => {
+    console.log(ctx.request.headers)
+    console.log(ctx.request.body)
+    try {
+        const [[db]] = await pool.execute(`SELECT facebook_user_id FROM user WHERE facebook_user_id = ?`, [ctx.request.body['id']])
+        if (!db['facebook_user_id']) {
+            await pool.execute(`
+                INSERT INTO user
+                    (facebook_user_id, firstname, lastname, email)
+                VALUES
+                    (?, ?, ?, ?)
+                `,[ctx.request.body['id'], ctx.request.body['first_name'], ctx.request.body['last_name'], ctx.request.body['email']])
+        }
+        let fbSessionId = ctx.request.body['id']
+        console.log(fbSessionId)
+        ctx.redirect('/')
+    } catch (err) {
+        console.log(err)
+    }
+});
 
-app.post('/', fb)
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(3000)
